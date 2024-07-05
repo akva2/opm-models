b1a6cc91cab649ba960edefe1ece5ecac64c5f77
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-07-05 14:41:12 +0200

    Parameters::printValues: remove unused TypeTag tparam

--- opm/models/utils/start.hh
+++ opm/models/utils/start.hh
@@ -360,7 +360,7 @@ static inline int start(int argc, char **argv,  bool registerParams=true)
             if (printParams) {
                 bool printSeparator = false;
                 if (printParams == 1 || !isatty(fileno(stdout))) {
-                    Parameters::printValues<TypeTag>();
+                    Parameters::printValues();
                     printSeparator = true;
                 }
                 else
