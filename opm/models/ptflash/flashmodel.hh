4b3194a947d81b08e40be95f3fc34661addf6194
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-07-05 17:49:51 +0200

    move EnableIntensiveQuantityCache parameter to TypeTag-less implementation

--- opm/models/ptflash/flashmodel.hh
+++ opm/models/ptflash/flashmodel.hh
@@ -143,12 +143,6 @@ struct EnableEnergy<TypeTag, TTag::FlashModel>
 
 namespace Opm::Parameters {
 
-// The updates of intensive quantities tend to be _very_ expensive for this
-// model, so let's try to minimize the number of required ones
-template<class TypeTag>
-struct EnableIntensiveQuantityCache<TypeTag, Properties::TTag::FlashModel>
-{ static constexpr bool value = true; };
-
 // since thermodynamic hints are basically free if the cache for intensive quantities is
 // enabled, and this model usually shows quite a performance improvment if they are
 // enabled, let's enable them by default.
@@ -269,6 +263,8 @@ public:
         Parameters::registerParam<TypeTag, Parameters::FlashTwoPhaseMethod>
             ("Method for solving vapor-liquid composition. Available options include: "
              "ssi, newton, ssi+newton");
+
+        Parameters::SetDefault<Parameters::EnableIntensiveQuantityCache>(true);
     }
 
     /*!
