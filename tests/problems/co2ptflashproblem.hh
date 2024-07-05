a8d86448c9b60b83ba9dcbac3f7b996310ebb628
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-07-05 17:49:51 +0200

    move NewtonTolerance parameter to TypeTag-less implementation

--- tests/problems/co2ptflashproblem.hh
+++ tests/problems/co2ptflashproblem.hh
@@ -229,13 +229,6 @@ struct LinearSolverTolerance<TypeTag, Properties::TTag::CO2PTBaseProblem>
 };
 
 template <class TypeTag>
-struct NewtonTolerance<TypeTag, Properties::TTag::CO2PTBaseProblem>
-{
-    using type = GetPropType<TypeTag, Properties::Scalar>;
-    static constexpr type value = 1e-3;
-};
-
-template <class TypeTag>
 struct NewtonTargetIterations<TypeTag, Properties::TTag::CO2PTBaseProblem>
 {
     using type = GetPropType<TypeTag, Properties::Scalar>;
@@ -406,6 +399,7 @@ public:
 
         Parameters::SetDefault<Parameters::EndTime<Scalar>>(60. * 60.);
         Parameters::SetDefault<Parameters::InitialTimeStepSize<Scalar>>(0.1 * 60. * 60.);
+        Parameters::SetDefault<Parameters::NewtonTolerance<Scalar>>(1e-3);
     }
 
     /*!
